dad394f5b01da6e5148a5c38d11fe3516317226e
MythiliSathish2019 <167442287+MythiliSathish2019@users.noreply.github.com>
2024-04-25 12:41:15 +0530

    Added a class to print the members of the family

---
+++ src/Family.java
@@ -0,0 +1,7 @@
+public class Family {
+    public static void printFamily(){
+        System.out.println("My Family!!!!");
+        System.out.println("Thvikshatra");
+        System.out.println("Sathish");
+    }
+}
