1dbaea32afd8e3cbf5a9e2ecb94359b7b78e2322
MythiliSathish2019 <167442287+MythiliSathish2019@users.noreply.github.com>
2024-04-25 13:26:50 +0530

    Added new class with static method to print list of friends

---
+++ src/Friends.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Friends {
+    public static void printFriends(ArrayList<String> friends){
+        System.out.println("My friends are");
+        for(String s:friends){
+            System.out.println(s);
+        }
+    }
+}
